05360b959b089d65dfd49f87049521755dbbfeb2
Eliezer Castro <eliezergino@gmail.com>
2024-01-15 11:19:20 -0300

    feat: add test search with google

---
+++ steps/search_test.js
@@ -0,0 +1,10 @@
+Feature('search');
+
+Scenario('test search with google', ({ I }) => {
+  I.amOnPage('/');
+  I.waitForElement({ xpath: '//input[@type="text"][contains(@name, "q")]' });
+  I.fillField({ xpath: '//input[@type="text"][contains(@name, "q")]' }, 'Unicamp');
+  I.waitForVisible({ xpath: '//input[@type="submit"][contains(@name, "btnK")]' });
+  I.click({ xpath: '//input[@type="submit"][contains(@name, "btnK")]' });
+  I.waitForText('| Unicamp')
+});
